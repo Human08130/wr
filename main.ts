147443b4e5f676ce54b56f095e599ae9967593d1
Human08130 <59336655+Human08130@users.noreply.github.com>
2021-05-11 15:18:08 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+music.playTone(220, music.beat(BeatFraction.Whole))
+music.playTone(247, music.beat(BeatFraction.Whole))
+music.playTone(262, music.beat(BeatFraction.Whole))
+music.playTone(294, music.beat(BeatFraction.Whole))
+music.playTone(330, music.beat(BeatFraction.Whole))
+music.playTone(262, music.beat(BeatFraction.Whole))
+music.playTone(330, music.beat(BeatFraction.Whole))
+music.rest(music.beat(BeatFraction.Whole))
+music.playTone(311, music.beat(BeatFraction.Whole))
+music.playTone(247, music.beat(BeatFraction.Whole))
+music.playTone(311, music.beat(BeatFraction.Whole))
+basic.forever(function () {
+	
+})
